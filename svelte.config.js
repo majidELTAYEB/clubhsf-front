8433dd09f71c0dd84d4d5fc3d195004fd0c36dd2
majid eltayeb <majid.eltayeb@epitech.eu>
2026-09-10 22:06:58 +0900

    i

---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import adapter from '@sveltejs/adapter-node';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    kit: {
+        adapter: adapter(),
+        experimental: {
+            instrumentation: {
+                server: true
+            }
+        }
+    }
+};
+
+export default config;
--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,44 @@
+// import { sentrySvelteKit } from '@sentry/sveltekit';
+// import tailwindcss from '@tailwindcss/vite';
+// import adapter from '@sveltejs/adapter-node';
+// import { sveltekit } from '@sveltejs/kit/vite';
+// import { defineConfig } from 'vite';
+
+// export default defineConfig({
+// 	plugins: [
+// 		sentrySvelteKit({
+// 			org: 'majido',
+// 			project: 'javascript-sveltekit'
+// 		}),
+// 		tailwindcss(),
+// 		sveltekit({
+// 			compilerOptions: {
+// 				runes: ({ filename }) =>
+// 					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+// 			},
+// 			adapter: adapter()
+// 		}),
+// 	],
+// });
+
+
 import { sentrySvelteKit } from '@sentry/sveltekit';
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-node';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		sentrySvelteKit({
-			org: 'majido',
-			project: 'javascript-sveltekit'
-		}),
-		tailwindcss(),
-		sveltekit({
-			compilerOptions: {
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
-			adapter: adapter()
-		}),
-	],
-	 server: {
-    allowedHosts: ['hierologic-overtart-sid.ngrok-free.dev'],
-  },
+    plugins: [
+        sentrySvelteKit({
+            org: 'majido',
+            project: 'javascript-sveltekit'
+        }),
+        tailwindcss(),
+        sveltekit({
+            compilerOptions: {
+                runes: ({ filename }) =>
+                    filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+            }
+        }),
+    ],
 });
