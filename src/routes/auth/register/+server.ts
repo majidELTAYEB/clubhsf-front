0810a0fae070954383bbb1ae95ff42434fa6659b
majid eltayeb <majid.eltayeb@epitech.eu>
2026-09-12 19:35:36 +0900

    WIP

--- src/routes/auth/register/+server.ts
+++ src/routes/auth/register/+server.ts
@@ -1,6 +1,6 @@
 import type { RequestHandler } from './$types';
 import { startAuth0Flow } from '$lib/features/auth/startAuth0Flow';
 
-export const GET: RequestHandler = async ({ cookies, url }) => {
-  return startAuth0Flow(url, cookies, 'signup');
+export const GET: RequestHandler = async ({ cookies, url, request }) => {
+  return startAuth0Flow(url, cookies, 'signup', request);
 };
