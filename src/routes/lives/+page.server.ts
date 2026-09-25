083ca18ed3b5371632b57a833141171fc6cb025d
majid eltayeb <majid.eltayeb@epitech.eu>
2026-09-25 21:10:44 +0900

    WIP

--- src/routes/lives/+page.server.ts
+++ src/routes/lives/+page.server.ts
@@ -1,19 +1,20 @@
-import { error} from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 
-
 export const load: PageServerLoad = async ({ fetch }) => {
-	const res = await fetch(`/api/livestreams`);
-	const resReplays = await fetch(`/api/livestreams/replays`);
+	const [res, resReplays] = await Promise.all([
+		fetch('/api/livestreams'),
+		fetch('/api/livestreams/replays')
+	]);
 
-	if (res.status === 404) {
-		throw error(404, "impossible de charger les lives");
-	}
 	if (!res.ok) {
-		throw error(res.status, "Impossible de charger les lives");
+		throw error(res.status, 'Impossible de charger les lives');
+	}
+	if (!resReplays.ok) {
+		throw error(resReplays.status, 'Impossible de charger les replays');
 	}
 
-	const lives = await res.json();
-	const replays = await resReplays.json();
+	const [lives, replays] = await Promise.all([res.json(), resReplays.json()]);
+
 	return { lives, replays };
 };
